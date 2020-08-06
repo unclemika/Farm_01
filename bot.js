a231226f93befcd13a8b8de2933415c2a757e4f7
unclemika <69279657+unclemika@users.noreply.github.com>
2020-08-06 04:19:00 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const myid = ['641668352045219862']
+const myid = ['409941554518687744']
 const client = new Discord.Client();
 const client2 = new Discord.Client();
 const client3 = new Discord.Client();
